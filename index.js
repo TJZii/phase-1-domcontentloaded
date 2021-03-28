0659744b1e5436fdabf526a6488d6629df0da910
ziitania <tjhaweszii@gmail.com>
2021-03-28 01:34:20 -0700

    commit tree 0dd22121ff9ec56baf5419521e337aa7c0368711

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+// document.addEventListener("DOMContentLoaded", function() {
+//     console.log("The DOM has loaded");
+// });
+
+document.addEventListener("DOMContentLoaded", function() {
+    textChanger()
+});
+
+function textChanger() {
+    document.getElementById(`text`).innerHTML = `This is really cool!`;
+}
